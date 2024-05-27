6660d2bfe97e9c408f04a694eeca287cf7fe835b
Mahesh Makani <mahesh@makani.dev>
2024-05-27 19:39:23 +0100

    26 current  2024-05-27 19:28:57  24.11.20240524.bfb7a88  6.6.31                          *

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    plasma-manager = {
+      url = "github:pjones/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.yorha-nix = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/yorha-nix/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
---
+++ modules/home-manager/git.nix
@@ -0,0 +1,10 @@
+{ pkgs, ...}:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Mahesh Makani";
+    userEmail = "mahesh@makani.dev";
+    includes = [{ path = "~/.gitconfig_local"; }];
+  };
+}
